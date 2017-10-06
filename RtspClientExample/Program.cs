6aae56ea0adb660a2203ee0657b048db0150e4db
Roger Hardiman <opensource@rjh.org.uk>
2017-10-06 06:45:03 +0100

    Improve Play, Pause and Quit control

--- RtspClientExample/Program.cs
+++ RtspClientExample/Program.cs
@@ -7,6 +7,8 @@ namespace RtspClientExample
 {
     class Program
     {
+        enum StreamMode { Play, Pause,  Stop };
+
         static void Main(string[] args)
         {
             //String url = "rtsp://192.168.1.128/ch1.h264";    // IPS
@@ -26,16 +28,15 @@ namespace RtspClientExample
             //String url = "rtsp://192.168.1.125/onvif-media/media.amp?profile=mobile_jpeg";
 
 
-            // Start a Task that continually connects to the RTSP Server until ENTER is pressed
-            bool want_video = true;
-            bool want_play = false;
-            bool want_pause = false;
+            DateTime time_check = DateTime.Now; // Holds the time 'Play' started or the time of the last NAL
+            StreamMode current_mode = StreamMode.Play;
+            StreamMode requested_mode = StreamMode.Play;
 
             // Create a RTSP Client. This is created in a Task so that it can connect (and re-connect)
             // in parallel with the User Interface (readline)
             Task rtspTask = new Task(delegate {
                 // Keep looping until want_video is false
-                while (want_video) {
+                while (requested_mode != StreamMode.Stop) {
                     RTSPClient c = new RTSPClient(url, RTSPClient.RTP_TRANSPORT.TCP);
                     c.Received_SPS32_PPS32 += (byte[] sps32, byte[] pps32) => {
                         Console.WriteLine("Received SPS and PPS from RTSP Client");
@@ -43,32 +44,47 @@ namespace RtspClientExample
                     };
                     c.Received_NAL32 += (byte[] nal32) => {
                         Console.WriteLine("Received NALs from RTSP Client");
+                        time_check = DateTime.Now;
                         // Pass this data into a H264 decoder
                     };
 
-                    // busy-wait  (//Todo. Change to a better implementation
+                    time_check = DateTime.Now;
+                    current_mode = StreamMode.Play;
+
+                    // busy-wait loop
+                    // Check Connection Status (on RTSP Socket.
+                    // Check when last NAL arrived
+                    // Check User Intetface requests
                     while ((c.CurrentStatus == RtspStatus.Connecting
                             || c.CurrentStatus == RtspStatus.Connected)
-                           && want_video == true) {
+                           && requested_mode != StreamMode.Stop) {
 
-                        if (want_play) {
+                        if (requested_mode == StreamMode.Play && current_mode != StreamMode.Play) {
                             c.Play();
-                            want_play = false;
+                            time_check = DateTime.Now;
+                            current_mode = StreamMode.Play;
                         }
-                        if (want_pause) {
+
+                        if (requested_mode == StreamMode.Pause && current_mode != StreamMode.Pause) {
                             c.Pause();
-                            want_pause = false;
+                            current_mode = StreamMode.Pause;
                         }
+
+                        if (current_mode == StreamMode.Play && (DateTime.Now - time_check).TotalSeconds > 10) {
+                            // No NALs received in the last 10 seconds. Assume the stream has failed. Disconnect & Reconnect
+                            current_mode = StreamMode.Stop;
+                            c.Stop(); // Stop closes the RTSP Socket. Status will change to Disconnected
+                        }
+
                         Thread.Sleep(250);
                     }
 
-                    // Either CurrentStatus is ConnectionFailed or is Disconnected
-                    // OR want_video is now false
+                    // User requested stop, or Status gives a connection error.
                     c.Stop();
 
 
                     // Wait 1 second before reconnecting, if we still want video
-                    if (want_video == true) Thread.Sleep(1000);
+                    if (requested_mode != StreamMode.Stop) Thread.Sleep(1000);
                 }
             });
             rtspTask.Start();
@@ -80,18 +96,18 @@ namespace RtspClientExample
             Console.WriteLine("Press Q then ENTER to quit");
 
             String readline = null;
-            while (want_video == true) {
+            while (requested_mode != StreamMode.Stop) {
                 readline = Console.ReadLine();
 
                 if (readline == null) {
                     // Avoid maxing out CPU on systems that instantly return null for ReadLine
                     Thread.Sleep(500);
                 } else if (readline.Equals("1")) {
-                    want_play = true;
+                    requested_mode = StreamMode.Play;
                 } else if (readline.Equals("2")) {
-                    want_pause = true;
+                    requested_mode = StreamMode.Pause;
                 } else if (readline.ToUpper().Equals("Q")) {
-                    want_video = false;
+                    requested_mode = StreamMode.Stop;
                 }
             }
 
